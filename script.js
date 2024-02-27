6cec6ffbf283f213f6fe38a57b4c68f584f33c83
Mohammad Raihan Gazi <gaziraihan505@gmail.com>
2024-02-27 20:48:01 +0600

    responsive navbar

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function toggleMenu() {
-    var menu = document.querySelector('.menu ul');
-    menu.classList.toggle('active');
-}
+// let menu = document.getElementById('menu');
+// let menuBtn = document.querySelector('.menuBtn');
+// let closeBtn = document.querySelector('.closeBtn')
+
+// function toggleMenu() {
+//     menu.classList.toggle('show');
+// }
+
+// menuBtn.addEventListener('click', toggleMenu);
+// closeBtn.addEventListener('click', toggleMenu);
